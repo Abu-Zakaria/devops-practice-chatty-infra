81044c934c48bdc621969fde61f60f8d35063cee
Abu Zakaria <abu.zakaria@metadesignsolutions.co.uk>
2023-07-12 01:00:16 +0600

    Add app and database servers

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,31 @@ resource "linode_instance" "load_balancer" {
 output "load_balancer_ip_address" {
 	value = linode_instance.load_balancer.ip_address
 }
+
+resource "linode_instance" "app01" {
+	label = "app01"
+	image = var.image
+	region = var.region
+	type = var.type
+	authorized_keys = [var.public_ssh_key]
+	root_pass = var.root_pass
+	tags = ["devops-practice-chatty"]
+}
+
+output "app01_ip_address" {
+	value = linode_instance.app01.ip_address
+}
+
+resource "linode_instance" "db01" {
+	label = "db01"
+	image = var.image
+	region = var.region
+	type = var.type
+	authorized_keys = [var.public_ssh_key]
+	root_pass = var.root_pass
+	tags = ["devops-practice-chatty"]
+}
+
+output "db01_ip_address" {
+	value = linode_instance.db01.ip_address
+}
